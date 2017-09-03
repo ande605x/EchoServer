7186927805300a1c55f865394e1b94e38ed3d9dc
Anders Thomsen <ande605x@edu.easj.dk>
2017-09-03 09:06:47 +0200

    Assingment C done

---
+++ EchoClient/Client.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EchoClient
+{
+    public class Client
+    {
+        public void Start()
+        {
+            using (TcpClient socket = new TcpClient("localhost", 7))
+            using (NetworkStream ns = socket.GetStream())
+            using (StreamReader sr = new StreamReader(ns))
+            using (StreamWriter sw = new StreamWriter(ns))
+            {
+                sw.WriteLine("Anders");
+                sw.Flush();
+                String line = sr.ReadLine();
+                Console.WriteLine(line);
+            }
+
+        }
+    }
+}
